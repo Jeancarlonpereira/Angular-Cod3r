175fc63b5536bccdd42ff09b8d31b79856df56b9
Jean Pereira <jeancarlo.npereira@gmail.com>
2020-09-07 20:25:15 -0300

    create component get-product

--- crud/frontend/src/app/app.module.ts
+++ crud/frontend/src/app/app.module.ts
@@ -22,7 +22,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import {HttpClientModule } from '@angular/common/http';
 import { FormsModule} from '@angular/forms'
 import { MatFormFieldModule} from '@angular/material/form-field'
-import {MatInputModule } from '@angular/material/input'
+import {MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
 
 
 @NgModule({
@@ -35,6 +36,7 @@ import {MatInputModule } from '@angular/material/input'
     ProductCrudComponent,
     RedDirective,
     ProductCreateComponent,
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
